920eff8858d95c1050e0143408fe5ef9c2495077
TanyaSoni29 <tanu139sona@gmail.com>
2025-07-20 00:30:54 +0530

    intial set up

---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation";
+
+export default function Page() {
+  return (
+    <div>
+      {" "}
+      <Navigation />
+      <h1>Your Account</h1>
+    </div>
+  );
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+
+export default function Page() {
+  return (
+    <div>
+      <Navigation />
+      <h1>Cabins</h1>
+    </div>
+  );
+}
+
+// we can call this any thing like Cabins Page but by convention we always call it as Page
