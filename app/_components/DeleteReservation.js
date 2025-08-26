4044908790bf9477527022c8ae34bea28aaa83be
TanyaSoni29 <tanu139sona@gmail.com>
2025-08-26 23:51:08 +0530

    delete reservations and useTransition hook

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,12 +1,40 @@
-import { TrashIcon } from '@heroicons/react/24/solid';
+"use client";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { deleteGuestReservation } from "../_lib/actions";
+import { useTransition } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
+  // here we can also write our server action and we have use that directive
+  // function deleteReservation() {
+  // "use server" like this but we don't do that because we want to place our server action in central place and this component may also become client component in future
+  //}
+  const [isPending, startTransition] = useTransition(); // isPending is flag when state updates it become true and this startTransition function where we wrap heavy state updates into. // all these are working like this because next js uses suspense boundaries behind the scenes means all navigation wrap inside the transition already for confusion please read notes
+
+  function handleDelete() {
+    if (confirm("Are you sure you want to delete this reservation"))
+      startTransition(() => deleteGuestReservation(bookingId)); // in this way we wrap // in react we use in heavy state transition but in next js we are using for server action
+  }
   return (
-    <button className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
-      <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-      <span className='mt-1'>Delete</span>
+    <button
+      onClick={handleDelete}
+      className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+    >
+      {!isPending ? (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </>
+      ) : (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   );
 }
 
 export default DeleteReservation;
+
+// another way of showing loading indicator now is useTransition Hook and we need this hook because we are not using the form now  this hook is introduced in React 18  as being so called as concurrent feature
+// use Transition allow us to mark a state update as a so-called transition and when the state update is marked as a transition by using this hook the state update will happen without blocking the ui which means ui remains responsive during re-render and we also get indication that state transition is happening this is can be very useful in vanilla react ui where multiple state updates happens that might block the ui so we can say we are doing so in the background so the ui stays responsive
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,6 +2,7 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 // by default we have access of formData when we use server action in action attribute of form does not need to pass that data to server action
 export async function updateGuest(formData) {
   // console.log("Server ACtions");
@@ -33,8 +34,29 @@ export async function updateGuest(formData) {
   // after running of server action data is updated in database but not in ui because of Router Cache in Browser hence to safe us from stale data we need to revalidate the cache manually
   // we know we have two type of revalidation one is time based and another one is manually means whenever we want we can revalidate the cache
   // revalidatePath("/account"); // we can left like this then all the data below this account route will revalidated means all data for home reservation profile all will that come under this account route will revalidated however this is too much so we can specify also so we use entire route
-  revalidatePath("/account/profile")
+  revalidatePath("/account/profile");
+}
 
+// delete Reservation server action
+// if we open network and check currently delete booking then if we go copy and copy as curl and then paste to terminal then we have access of currently deleted booking id now any one can change this id and delete our reservation so here we need extra protection (leakage of bookingId - last as raw data) to protect we want only guest can delete its booking get all the bookings by users then check if the id that has been passed is same as bookings id that user have then delete other wise show error
+export async function deleteGuestReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+  // we have revalidate Tag (for some pieces of data that we fetched) also to go more specifically but here it is better way and easy as well and also note that we are not using native fetch function we are using supabase for data so it is harder to use revalidatePath or we can say it is impossible might be
+  revalidatePath("/account/reservations");
 }
 
 // this is first action that this action file contains why we defining this we will learn letter
@@ -49,4 +71,4 @@ export async function signOutAction() {
 }
 
 // now let add a loading indicator for user so that user now form is correctly updated now for that tracking react give us a new Hook that is useFormStatus actually this hook is part of React Dom not just of React
-// this new hook cannot be imported where the form is placed it is imported inside a component which will render inside the form 
+// this new hook cannot be imported where the form is placed it is imported inside a component which will render inside the form
