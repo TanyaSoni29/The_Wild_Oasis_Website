33f4d5609b42431d1c1856b160e4ce4d25d780da
TanyaSoni29 <tanu139sona@gmail.com>
2025-09-21 15:07:22 +0530

    formating

--- app/_components/ReservationList.js
+++ app/_components/ReservationList.js
@@ -3,6 +3,7 @@
 import { useOptimistic } from "react";
 import ReservationCard from "./ReservationCard";
 import { deleteGuestReservation } from "../_lib/actions";
+
 export default function ReservationList({ bookings }) {
   const [optimisticBookings, optimisticDelete] = useOptimistic(
     bookings,
@@ -10,10 +11,12 @@ export default function ReservationList({ bookings }) {
       return currentBookings.filter((booking) => booking.id !== bookingId); // in setter function that we pass we will get that bookingId as well
     }
   ); // here we need to think about two state one is actual state and one is state after that async operation (optimistic state) first we pass current state after second we pass updated function and we get two state as result as in useState one is optimistic state another is setter function now inside the Reservation Card we have deleteReservation component in that component we specify the handler now we have use that handler here and remove from there it is passed from this component optimisticDelete triggering that function that we pass in useOptimistic hook
+  
   async function handleDelete(bookingId) {
     optimisticDelete(bookingId); // here we call for function and pass id so that it will know for which we are calling the function
     await deleteGuestReservation(bookingId);
   }
+  
   return (
     <ul className="space-y-6">
       {optimisticBookings.map((booking) => (
